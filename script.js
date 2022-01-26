e2d8aa77ddeac2aa6b35fd5aa32b7a5179055e29
Anna <annawebbstewart@gmail.com>
2022-01-26 21:19:47 +0000

    Script made neater, some css added

--- script.js
+++ script.js
@@ -2,62 +2,68 @@ let playerScore = 0;
 let computerScore = 0;
 let updatedPlayerScore;
 let updatedComputerScore;
+let refresh;
 const rck = document.querySelector('#rock');
 const ppr = document.querySelector('#paper');
 const scrs = document.querySelector('#scissors');
 const buttons = document.querySelectorAll('button');
 const options = ["rock", "paper", "scissors"];
-
-
-
+const playerAnswer = document.querySelector('#player');
+const computerAnswer = document.querySelector('#computer');
+const vs = document.querySelector('#vs');
+const body = document.querySelector('body');
 const results = document.querySelector('#results');
 
+
 rck.addEventListener('click', () => {
-    const playerChoice = 'rock';
-    const computerChoice = options[Math.floor(Math.random()*options.length)];
-    console.log(playerChoice + ' ' + computerChoice);
-    playRound(playerChoice, computerChoice);
+    setupRound('rock');
 });
 
 ppr.addEventListener('click', () => {
-    const playerChoice = 'paper';
-    const computerChoice = options[Math.floor(Math.random()*options.length)];
-    console.log(playerChoice + computerChoice);
-    playRound(playerChoice, computerChoice);
+    setupRound('paper');
 });
 
 scrs.addEventListener('click', () => {
-    const playerChoice = 'scissors';
-    const computerChoice = options[Math.floor(Math.random()*options.length)];
-    console.log(playerChoice + computerChoice);
-    playRound(playerChoice, computerChoice);
+    setupRound('scissors');
 });
+
+function setupRound(playerChoice) {
+
+    if (playerScore === 5 || computerScore === 5 ) {
+        return;
+    }
+    const computerChoice = options[Math.floor(Math.random()*options.length)];
+    playRound(playerChoice, computerChoice);
+}
 
 function playRound(playerChoice, computerChoice) {
 
     if (playerChoice === computerChoice) {
-        //roundResult = "draw";
         console.log("You both said the same!")
-        results.textContent = `You: ${playerScore}. Computer: ${computerScore}.`
     }
-    else if ((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'paper' && computerChoice === 'rock') ||(playerChoice === 'scissors' && computerChoice === 'paper') ) {
-        //roundResult = "win";
+    else if ((playerChoice === 'rock' && computerChoice === 'scissors') || 
+                (playerChoice === 'paper' && computerChoice === 'rock') ||
+                (playerChoice === 'scissors' && computerChoice === 'paper') ) {
         playerScore += 1;
-        updatedPlayerScore = playerScore;
         console.log("You win this one!");
-        results.textContent =`You: ${playerScore}. Computer: ${computerScore}.`;
-        if (updatedPlayerScore === 5) {
-            alert("You win!");
-         }
     }
-    else if ((playerChoice === 'rock' && computerChoice === 'paper') || (playerChoice === 'paper' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'rock')) {
-        //roundResult = "lose";
+    else if ((playerChoice === 'rock' && computerChoice === 'paper') || 
+                (playerChoice === 'paper' && computerChoice === 'scissors') ||
+                 (playerChoice === 'scissors' && computerChoice === 'rock')) {
         computerScore += 1;
-        updatedComputerScore = computerScore;
         console.log("Too bad, computer wins!");
-        results.textContent =`You: ${playerScore}. Computer: ${computerScore}.`;
-        if (updatedComputerScore === 5) {
-            alert("You lose!");
-        }
     }
+
+    playerAnswer.textContent = playerChoice;
+    vs.textContent = 'vs';
+    computerAnswer.textContent = computerChoice;
+    results.textContent =`You: ${playerScore}. Computer: ${computerScore}.`;
+
+    if (playerScore === 5 ) {
+        alert("You win!");
+    }
+    if (computerScore === 5) {
+        alert("You lose!");
+    }
+
 }
